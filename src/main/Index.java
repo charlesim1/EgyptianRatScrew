cb0029b5df31f4e8140d25353f7ea83f34036ce9
charlesim1 <charles.im3@gmail.com>
2019-10-06 06:46:21 -0500

    Added beginning input prompts for the user

--- src/main/Index.java
+++ src/main/Index.java
@@ -1,7 +1,15 @@
 package main;
 
+import java.util.Scanner;
+
 public class Index {
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 		
+		System.out.print("How many computers do you wish to play with? (can select between 1 - 3) : ");
+		int numComputers = input.nextInt();
+		
+		System.out.print("\nWhat difficulty do you want to play with? (1 being the easiest and 5 being the hardest: \n"); 
+		int difficulty = input.nextInt();
 	}
 }
